7a59eb2f1baeef2f00e359be594ef34511aa5797
ANGELINA ROSE VARGHESE <angelinarosevarghese2028@cs.sjcetpalai.ac.in>
2025-12-01 00:56:08 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,128 +1,61 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-	int count,i,preferences,front=-1,rear=-1,element=0;
-	printf("Enter the maximun size for the for queue: ");
-	scanf("%d",&count);
-	int num[count];
-	printf("What do you want: \n 1.Input element\n 2.Delete an element \n 3.Exit");
-	scanf("%d",&preferences);
-	while(preferences!=3)
-	{
-		if(preferences==1)
-		{
-			if(rear==count-1)
-			{
-				printf("Overflow\n");
-			}
-			else
-			{
-				printf("Enter the element to be added: ");
-                		scanf("%d", &element);
-				if(front == -1) front = 0; 
-				rear++;
-				num[rear] = element;
-				printf("The elements in the queue are: \n");
-				for(i = front; i <= rear; i++)
-				{
-				    printf("%d\t", num[i]);
-				}
-				printf("\n");
-				
-			}
-		}
-		else if(preferences==2)
-		{
-			if((rear<front)||(front==-1))
-			{
-				printf("Underflow\n");
-			}
-			else
-			{
-				element=0;
-				element=num[front];
-				printf("Deleted element is %d \n",element);
-				front=front+1;
-				if (front > rear)
-				{ 
-					front = -1;
-					rear = -1;
-				}
-				if(front != -1) 
-				{
-					printf("The elements in the queue are: \n");
-					for(i=front;i<=rear;i++)
-					{
-						printf("%d\t",num[i]);
-					}
-				}
-			}
-		}
-		else if(preferences==3)
-		break;
-		else
-		{
-			printf("Invalid");
-		}
-		printf("\n What do you want: \n 1.Input element\n 2.Delete an element \n 3.Exit\n");
-		scanf("%d",&preferences);
-		printf("\n");
-	}
-	return 0;
+    int count, i, choice, front = -1, rear = -1, element;
+    printf("Enter max size of queue: ");
+    scanf("%d", &count);
+    int num[count];
+    while (1)
+    {
+        printf("\n1. Insert\n2. Delete\n3. Exit\nEnter choice: ");
+        scanf("%d", &choice);
+        if (choice == 1)    // INSERT
+        {
+            if (rear == count - 1)
+            {
+                printf("Overflow\n");
+            }
+            else
+            {
+                printf("Enter element: ");
+                scanf("%d", &element);
+                if (front == -1) 
+                    front = 0;
+                num[++rear] = element;
+            }
+        }
+        else if (choice == 2)   // DELETE
+        {
+            if (front == -1 || front > rear)
+            {
+                printf("Underflow\n");
+            }
+            else
+            {
+                printf("Deleted: %d\n", num[front++]);
+                if (front > rear)
+                    front = rear = -1;
+            }
+        }
+        else if (choice == 3)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid choice\n");
+        }
+        if (front != -1)
+        {
+            printf("Queue: ");
+            for (i = front; i <= rear; i++)
+                printf("%d ", num[i]);
+            printf("\n");
+        }
+        else
+        {
+            printf("Queue is empty\n");
+        }
+    }
+    return 0;
 }
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
